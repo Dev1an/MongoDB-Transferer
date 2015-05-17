39d43c5637536b5ecd9ae2b7f72963355f3c4cef
Devian <info@devian.tk>
2015-05-17 11:34:31 +0200

    Initial Commit

---
+++ Meteor Database Transferer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Meteor Database Transferer
+//
+//  Created by Damiaan Dufaux on 17/05/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
